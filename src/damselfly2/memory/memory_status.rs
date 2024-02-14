8d74e3a1db991b18e79f3c698c8c044c0dd63197
Saurav Matheril <saurav.matheril@hp.com>
2024-02-14 17:12:30 +0800

    memory canvas, block

--- src/damselfly2/memory/memory_status.rs
+++ src/damselfly2/memory/memory_status.rs
@@ -2,10 +2,8 @@ use std::sync::Arc;
 
 #[derive(PartialEq, Debug, Clone)]
 pub enum MemoryStatus {
-    // parent block, allocation size from parent, callstack
-    Allocated(usize, usize, Arc<String>),
-    // parent block, callstack
-    PartiallyAllocated(usize, Arc<String>),
-    Free(Arc<String>),
+    Allocated,
+    PartiallyAllocated,
+    Free,
     Unused,
 }
--- src/damselfly2/viewer/memory_canvas.rs
+++ src/damselfly2/viewer/memory_canvas.rs
@@ -1,48 +1,62 @@
+use std::cmp::{max, min};
 use std::iter::StepBy;
 use std::ops::Range;
 use rust_lapper::Lapper;
 use crate::damselfly2::consts::DEFAULT_MEMORYSPAN;
 use crate::damselfly2::memory::memory_status::MemoryStatus;
-use crate::damselfly2::memory::memory_update::{MemoryUpdate, MemoryUpdateType};
+use crate::damselfly2::memory::memory_update::MemoryUpdateType;
+use crate::damselfly2::update_interval::update_interval_sorter::UpdateIntervalSorter;
 use crate::damselfly2::update_interval::UpdateInterval;
 
-pub struct Bucket {
-    free_span: (usize, usize),
-    bucket_size: usize,
-    bucket_status: MemoryStatus,
+pub struct Block {
+    block_bounds: (usize, usize),
+    remaining_bytes: usize,
+    block_status: MemoryStatus,
 }
 
-impl Bucket {
-    pub fn new(bucket_index: usize, bucket_size: usize) -> Bucket {
-        Bucket {
-            free_span: (bucket_index, bucket_index + bucket_size),
-            bucket_size,
-            bucket_status: MemoryStatus::Unused,
+impl Block {
+    pub fn new(block_index: usize, block_size: usize) -> Block {
+        Block {
+            block_bounds: (block_index, block_index + block_size),
+            remaining_bytes: block_size,
+            block_status: MemoryStatus::Unused,
         }
     }
 
-    pub fn update_block(&mut self, update_interval: UpdateInterval) {
-        if update_interval.start <= self.free_span.0 && update_interval.stop >= self.free_span.1 {
-            self.apply_update(update_interval.val);
-            return;
+    pub fn update_block(&mut self, update_interval: &UpdateInterval) {
+        let constrained_bounds = (
+            max(self.block_bounds.0, update_interval.start),
+            min(self.block_bounds.1, update_interval.stop)
+        );
+        let bytes_consumed = constrained_bounds.1 - constrained_bounds.0;
+        match update_interval.val {
+            MemoryUpdateType::Allocation(_) => {
+                self.remaining_bytes = self.remaining_bytes.saturating_sub(bytes_consumed);
+            }
+            MemoryUpdateType::Free(_) => {
+                self.remaining_bytes = self.remaining_bytes.saturating_add(bytes_consumed)
+                    .clamp(usize::MIN, self.block_bounds.1 - self.block_bounds.0);
+            }
         }
-        self.apply_partial_update(update_interval.start, update_interval.stop, update_interval.val);
+        self.remaining_bytes = self.remaining_bytes.saturating_sub(bytes_consumed);
+        self.update_block_status(update_interval.clone().val);
     }
 
-    fn apply_update(&mut self, memory_update: MemoryUpdateType) {
-        match memory_update {
-            MemoryUpdateType::Allocation(allocation) => self.bucket_status
-                = MemoryStatus::Allocated(allocation.get_absolute_address(),
-                                          allocation.get_absolute_size(),
-                                          allocation.get_callstack()),
-            MemoryUpdateType::Free(free) => self.bucket_status
-                = MemoryStatus::Free(free.get_callstack()),
-        }
+    pub fn get_block_start(&self) -> usize {
+        self.block_bounds.0
     }
 
-    fn apply_partial_update(&mut self, update_start: usize, update_stop: usize, update: MemoryUpdateType) {
-        if matches!(self.bucket_status, MemoryStatus::PartiallyAllocated(..)) {
-            let current_state_size = self.bucket_
+    pub fn get_block_stop(&self) -> usize {
+        self.block_bounds.1
+    }
+
+    fn update_block_status(&mut self, memory_update: MemoryUpdateType) {
+        if self.remaining_bytes == 0 {
+            self.block_status = MemoryStatus::Free;
+        } else if self.remaining_bytes < (self.block_bounds.1 - self.block_bounds.0) {
+            self.block_status = MemoryStatus::PartiallyAllocated;
+        } else if self.remaining_bytes >= (self.block_bounds.1 - self.block_bounds.0) {
+            self.block_status = MemoryStatus::Allocated;
         }
     }
 }
@@ -51,6 +65,7 @@ pub struct MemoryCanvas {
     block_size: usize,
     start: usize,
     stop: usize,
+    blocks: Vec<Block>
 }
 
 impl MemoryCanvas {
@@ -59,17 +74,28 @@ impl MemoryCanvas {
             block_size: 1,
             start: 0,
             stop: DEFAULT_MEMORYSPAN,
+            blocks: Vec::new(),
         }
     }
 
-    pub fn load_intervals(&self, update_intervals: Vec<UpdateInterval>) {
+    pub fn load_intervals(&mut self, update_intervals: Vec<UpdateInterval>) {
         let lapper = Lapper::new(update_intervals);
-        let buckets = Vec::new();
-        let visible_blocks = self.get_block_iter();
-        for block_start in visible_blocks {
-            let overlapping_operations
-                = lapper.find(block_start, block_start + self.block_size);
+        self.insert_blocks();
+        for mut block in self.blocks {
+            let mut overlapping_operations
+                = lapper.find(block.get_block_start(), block.get_block_stop())
+                        .collect::<Vec<&UpdateInterval>>();
+            UpdateIntervalSorter::sort_by_timestamp(&mut overlapping_operations);
+            for update_interval in overlapping_operations {
+                block.update_block(update_interval);
+            }
+        }
+    }
 
+    fn insert_blocks(&mut self) {
+        for block in self.get_block_iter() {
+            let address = self.start + block;
+            self.blocks.push(Block::new(address, self.block_size));
         }
     }
 
