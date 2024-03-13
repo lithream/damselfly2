c7d07502a7910985540ffc782849417e354782fe
Saurav Matheril <masa192@hpsglux20.ijp.sgp.rd.hpicorp.net>
2024-03-13 12:39:59 +0800

    map working but all red

---
+++ src/GraphSliderComponent.tsx
@@ -0,0 +1,63 @@
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
+import MuiInput from '@mui/material/Input';
+
+interface SliderProps {
+    xClick: number;
+    setXClick: (x: number) => void;
+}
+
+const Input = styled(MuiInput)`
+    width: 42px;
+`;
+
+function Slider({ xClick, setXClick }: SliderProps) {
+    const handleSliderChange = (event: Event, newValue: number | number[]) => {
+        setXClick(newValue as number);
+    };
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setXClick(event.target.value === '' ? 0 : Number(event.target.value));
+    };
+
+    const handleBlur = () => {
+        if (xClick < 0) {
+            setXClick(0);
+        }
+    }
+
+    return (
+        <Box sx={{ width: 250 }}>
+            <Typography id="input-slider" gutterBottom>
+                Time
+            </Typography>
+            <Grid item xs>
+                <Slider
+                    value={typeof xClick === "number" ? xClick : 0}
+                    onChange={handleSliderChange}
+                    aria-labelledby="input-slider"
+                />
+            </Grid>
+            <Grid item>
+                <Input
+                    value={xClick}
+                    size="small"
+                    onChange={handleInputChange}
+                    onBlur={handleBlur}
+                    inputProps={{
+                        step: 10,
+                        min: 0,
+                        max: 100,
+                        type: 'number',
+                        'aria-labelledby': 'input-slider',
+                    }}
+                />
+            </Grid>
+        </Box>
+    );
+}
+
+export default Slider;
